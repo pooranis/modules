5775339e2c7f5940595bf49172a8e6d45bacd41b
pooranis <pooranis@users.noreply.github.com>
2018-06-16 09:55:47 -0400

    remove print statement

--- R/import.r
+++ R/import.r
@@ -80,7 +80,6 @@ import_ = function (module, attach, attach_operators = TRUE, doc, module_name=NU
   }
 
   if (!missing(attach) && attach) {
-    print(search())
     if (!is.null(module_name)) {
       mlpath <- paste0("module:", module_name)
       if (length(ml <- grep(mlpath, search(), value=T)) > 0) {
